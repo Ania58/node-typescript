6c5e4f846b60e57199ae88bf4af9cd0134bbd409
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2025-06-07 23:27:23 +0200

    create a basic app.ts setup and server to test out

---
+++ app.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    console.log(req.method);
+    res.json( { message: 'Hello World' });
+});
+
+app.listen(3000);
