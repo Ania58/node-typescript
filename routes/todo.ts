38c7bdd0ea1f41a1412b503b203c8d98ea5969b0
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2025-06-08 19:05:26 +0200

    Prepare todo.ts for the routes

---
+++ routes/todo.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import { getTodos, getTodo, addTodo, updateTodo, removeTodo } from '../data.js';
+
+const router = express.Router();
+
+
+
+
+
+export default router;
