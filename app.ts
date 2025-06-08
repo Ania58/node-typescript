382696b71a1040c0413146eb991883dbbc643489
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2025-06-08 19:11:25 +0200

    Prepare app.ts with the necessary imports, app.use for parsing the body, using the routes and handling errors with a middleware

--- app.ts
+++ app.ts
@@ -1,10 +1,13 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import todoRoutes from './routes/todo.js';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    console.log(req.method);
-    res.json( { message: 'Hello World' });
-});
+app.use(express.json());
+app.use(todoRoutes);
 
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    res.status(500).json( { message: 'An error occurred', error: err.message });
+    next();
+})
 app.listen(3000);
